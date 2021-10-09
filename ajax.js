c701dc760d951c4a2ccc577bb0961539fc86f4e4
samuraiht <aauioe@gmail.com>
2021-10-09 09:47:23 +0000

    最適化

--- ajax.js
+++ ajax.js
@@ -1,18 +1,18 @@
 window.onload = () => {
 // GET / POST メソッドの実装の例
   async function fetchData(requestURL, getQuery = '', postQuery = '') {
+    const ispost = postQuery != '';
 // 既定のオプションには * が付いています
     const data = {
-      method: postQuery != '' ? 'POST' : 'GET',// *GET, POST, PUT, DELETE, etc.
-      headers: {'Content-Type': postQuery != '' ? 'application/x-www-form-urlencoded' : 'text/plain'},
-
+      method: ispost ? 'POST' : 'GET',// *GET, POST, PUT, DELETE, etc.
+      headers: {'Content-Type': ispost ? 'application/x-www-form-urlencoded' : 'text/plain'},
       mode: 'cors',// no-cors, *cors, same-origin
       cache: 'no-cache',// *default, no-cache, reload, force-cache, only-if-cached
       credentials: 'same-origin',// include, *same-origin, omit
       redirect: 'follow',// manual, *follow, error
       referrerPolicy: 'no-referrer'// no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
     };
-    if(postQuery != '') data.body = postQuery;// 本文のデータ型は "Content-Type" ヘッダーと一致する必要があります
+    if(ispost) data.body = postQuery;// 本文のデータ型は "Content-Type" ヘッダーと一致する必要があります
 
     const response = await fetch(requestURL + (getQuery != '' ? '?' + getQuery : ''), data);
     return response.json();// レスポンスの JSON を解析
@@ -23,25 +23,25 @@ window.onload = () => {
     const text = document.getElementById('response');
     switch(data.result) {
       case 0:
-        text.textContent = data.count;
+        text.textContent = '在庫は' + data.count + '個です。';
         break;
       case 1:
-        text.textContent = '存在しない品目です';
+        text.textContent = '存在しない品目です。';
         break;
       case 2:
-        text.textContent = 'パラメーターが正しくありません';
+        text.textContent = 'パラメーターが正しくありません。';
     }
   }
 
 // GETの呼び出し例
   document.getElementById('get').onclick = e => {
     e.preventDefault();//既定の動作(formの送信など)を停止
-    fetchData('ajax.php', 'name=' + document.getElementById('fruit').value).then(data => { showResult(data); });
+    fetchData('ajax.php', 'name=' + document.getElementById('item').value).then(data => { showResult(data); });
   };
 
 // POSTの呼び出し例
   document.getElementById('post').onclick = e => {
     e.preventDefault();
-    fetchData('ajax.php', '', 'name=' + document.getElementById('fruit').value).then(data => { showResult(data); });
+    fetchData('ajax.php', '', 'name=' + document.getElementById('item').value).then(data => { showResult(data); });
   };
 };
--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 <html lang="ja">
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta name="viewport" content="width=device-width">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>非同期通信とデータベース</title>
 	<script src="ajax.js"></script>
 </head>
 <body>
 <form>
-	<input type="text" id="fruit" value="Apple">
+	<input type="text" id="item" value="">
 	<div id="response"></div>
 	<button id="get">GETで送信テスト</button>
 	<button id="post">POSTで送信テスト</button>
